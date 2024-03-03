35f4aef3e104a5d774c34e09fdc62635d8ffd674
student <Vicatrin@gmail.com>
2024-03-03 14:44:26 +0200

    множення віднімання та додавання a b c

---
+++ milka.py
@@ -0,0 +1,7 @@
+a = int(input('ведіть перше число:'))
+b = int(input('Введіть друге число:'))
+c = int(input('Введіть трете число:'))
+
+print(f'{a} + {b} + {c}= {a + b - c}')
+print(f'{a} - {b} - {c}= {a - b - c}')
+print(f'{a} * {b} * {c}= {a * b * c}')
