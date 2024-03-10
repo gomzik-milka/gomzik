d90c8815925982a7cd74c711ca6194a4d5dae6cd
student <Vicatrin@gmail.com>
2024-03-10 15:04:38 +0200

    Create class character

---
+++ lesson2/student.py
@@ -0,0 +1,30 @@
+class Student:
+    name = ''
+    group = ''
+    age = 0
+    grades = []
+    gra = 0
+
+    def __init__(self, name, group='', age=0, grades=()):
+        self.name = name
+        self.group = group
+        self.age = age
+        self.grades = grades
+        if grades:
+            self.gra = round(sum(self.grades) / len(grades), 2)
+
+    def show_info(self):
+        print(f'{self.name} ({self.age} років), группа {self.group}')
+        print(f'Середній бал: {self.gra} ({self.grades})')
+
+
+student1 = Student(name='Dave', group='B2121', age=14, grades=[8, 7, 9, 10, 8, 8, 6])
+
+student2 = Student('John')
+
+student2.group = 'B2121'
+student2.age = 15
+student2.grades = [6, 6, 5, 7, 6, 6, 8]
+
+student1.show_info()
+student2.show_info()
