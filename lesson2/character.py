721348d68a28ef24ec0f38b9311b55b6bb9e0411
student <Vicatrin@gmail.com>
2024-03-10 15:04:00 +0200

    Create class character

---
+++ lesson2/character.py
@@ -0,0 +1,30 @@
+class Character:
+    name = ''
+    health = 100
+    damage = 1
+    defense = 0
+
+    def __init__(self, name, health=100, damage=1, defence=0):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.defense = defence
+
+    def show_info(self):
+        print(f' = {self.name} =\n'
+              f' Health: {self.health}\n'
+              f' Damage: {self.damage}\n'
+              f' Defence: {self.defense}\n')
+
+    def __str__(self):
+        return f' = {self.name} =\n' \
+               f' Health: {self.health}\n' \
+               f' Damage: {self.damage}\n' \
+               f' Defence: {self.defense}\n'
+    def take_damage(self, damage):
+        self.health -= damage
+        return damage
+
+    def attack(self, target):
+        return target.take_damage(self.damage)
+    def is.alife
--- main.py
+++
@@ -1,9 +0,0 @@
-print('Hello world!')
-
-print(2 + 2)
-
-print('i love you')
-
-print('save me')
-
-print('i love borsh')
