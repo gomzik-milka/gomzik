4e003f235c42301cb9d55a32ab43b23547c780ff
student <Vicatrin@gmail.com>
2024-03-17 15:50:32 +0200

    berserk

--- Lesson3/berserk.py
+++ Lesson3/berserk.py
@@ -4,7 +4,7 @@ from lesson2.character import Character
 class berserk(Character):
     def __int__(self, name, health=100, damage=1, defence=0):
         super().__init__(name, health, damage, defence)
-        def .max_health = health
+        self.max_health = health
 
         def attack(self, target):
             additional_damage = (1 - self.health / self.max_health) * self.damage
