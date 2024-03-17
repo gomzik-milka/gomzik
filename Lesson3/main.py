2f16b21fb0a7714e59e4d9f6a790bff49cdff0ac
student <Vicatrin@gmail.com>
2024-03-17 14:53:29 +0200

    berserk

--- Lesson3/main.py
+++ Lesson3/main.py
@@ -1,13 +1,14 @@
-from character import Character
+from lesson2.character import Character
+from berserk import berserk
 
 
 player1 = Character('Vasya')
 player1.show_info()
-player2 = Character('Petya', damage=50)
+
+player2 = berserk('Petya', damage=50)
 print(player2)
 
-while True:
-    if player1.is_alive() and player2.is_alive():
+while player1.is_alive() and player2.is_alive():
         p1_damage = player1.attack(player2)
         p2_damage = player2.attack(player1)
         print(f'{player2.name} attack {player1.name} '
@@ -15,5 +16,5 @@ while True:
         print(f'{player1.name} attack {player2.name} '
               f'1 наніс {p1_damage} шкоди')
         print(player2, player1, sep='\n')
-    else:
-        break
+
+print(player2, player1, sep='\n')
